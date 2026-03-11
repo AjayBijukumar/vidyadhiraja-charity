62d215efb515ca5bed4b4201085728329559a091
AjayBijukumar <techzspot85@gmail.com>
2026-03-11 21:00:41 +0530

    Fix: Remove hardcoded database credentials

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ const prayerRoutes = require('./routes/prayer');
 // Import the public WhatsApp routes
 const whatsappRoutes = require('./routes/whatsapp');
 
-// MongoDB connection string
-const MONGODB_URI = 'mongodb://ajaybijukumar308_db_user:73HpqSFTQmAo84jH@ac-yow4amz-shard-00-00.peiimob.mongodb.net:27017,ac-yow4amz-shard-00-01.peiimob.mongodb.net:27017,ac-yow4amz-shard-00-02.peiimob.mongodb.net:27017/charity_db?ssl=true&replicaSet=atlas-iujpzd-shard-0&authSource=admin&appName=Cluster0n';
+// DB connection string
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // Connection options
 const connectionOptions = {
